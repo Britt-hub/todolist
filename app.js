7057e653179091525ddf4721cbd52afb27a4907f
Britt-hub <britloy91@gmail.com>
2021-07-24 10:45:54 -0400

    Add list file and view folder

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function (req, res) {
-        
-        var today = new Date();
-        var currentDay = today.getDay();
 
-        if (currentDay === 6 || currentDay === 0) {
-            res.write("<h1>Yay!! It's the weekend!</h1>");
-        } else {
-            res.sendFile(__dirname + "/index.html");
-        }
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
 
-    });
+    if (currentDay === 6 || currentDay === 0) {
+        day = "Weekend";        
+    } else {
+        day = "Weekday"; 
+    }
+
+    res.render('list', {kindOfDay: day});
+
+});
 
 app.listen(3000, function () {
     console.log("Server is running on 3000!")
