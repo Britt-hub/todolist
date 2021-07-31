96f625fa56e4d473b6ee97b4db8d626e7ffef82c
Britt-hub <britloy91@gmail.com>
2021-07-31 10:36:45 -0400

    creating views folder

--- app.js
+++ app.js
@@ -1,53 +1,54 @@
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = ["Wake up at 6am", "Drink water", "Study for 1 hour", "Make Coffee"];
+let workItems = [];
+
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    let today = new Date();
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-            case 1:
-                day = "Monday"
-            break;
-            case 2:
-                day = "Tuesday"
-            break;
-            case 3:
-                day = "Wednesday"
-            break;
-            case 4:
-                day = "Thursday"
-            break;
-            case 5:
-                day = "Friday"
-            break;
-            case 6:
-                day = "Saturday"
-            break;
-        default:
-       console.log("Error: current day is equal to: " + currentDay);
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        month: 'long',
+    };
+    
+    let day = today.toLocaleDateString("en-US", options);
+    
+    res.render('list', {listTitle: day, newListItems: items});
+});
+
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work List") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
     }
-    // if (currentDay === 6 || currentDay === 0) {
-    //     day = "Weekend";        
-    // } else {
-    //     day = "Weekday"; 
-    // }
+});
 
-    res.render('list', {kindOfDay: day});
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+       
+});
 
+app.get("/about", function (req, res) {
+    res.render("about");
 });
 
 app.listen(3000, function () {
-    console.log("Server is running on 3000!")
+    console.log("Server is running on Andre 3000!")
 });
